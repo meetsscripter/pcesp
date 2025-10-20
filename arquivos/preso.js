e7013f7b429918521c581bce033f9da4c4f1e0e5
meets <69513097+meetsscripter@users.noreply.github.com>
2025-10-19 23:29:11 -0300

    Update preso.js

--- arquivos/preso.js
+++ arquivos/preso.js
@@ -71,7 +71,7 @@ document.getElementById("discordLogin").addEventListener("click", async e => {
 
     if (isMember) {
       // Mantendo lógica antiga
-      document.querySelector('[name="responsavel"]').value = `<@${user.id}>`;
+      document.querySelector('[name="investigador"]').value = `<@${user.id}>`;
       document.getElementById("discordLogin").style.display = "none";
       loginStatus.style.display = "none";
       document.querySelector('.form-section').style.display = "block";
@@ -255,4 +255,5 @@ document.getElementById('prisonerForm').addEventListener('submit', async e => {
     btn.disabled = false;
   }
 });
+
 
